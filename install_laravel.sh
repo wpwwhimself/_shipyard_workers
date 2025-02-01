01859cb05cdbb3f209a19afb1ba6424092683dd8
Wojciech Przybyła <contact@wpww.pl>
2025-02-01 20:31:03 +0100

    ✨ created laravel builder

---
+++ install_laravel.sh
@@ -0,0 +1,43 @@
+# Script for installing Laravel application cloned from git
+# Run from project directory
+
+heading () {
+  echo -e "\033[1m$1\033[0m"
+}
+
+################
+
+heading "🔨 Configuring repository..."
+
+cp .env.example .env
+
+sed -i '' 's/APP_ENV=local/APP_ENV=stage/' .env
+sed -i '' 's/APP_LOCALE=en/APP_LOCALE=pl/' .env
+
+read -p "Enter database name: " DBNAME
+sed -i '' "s/DB_DATABASE=.*/DB_DATABASE=${DBNAME}/" .env
+read -p "Enter database name: " DBNAME
+sed -i '' "s/DB_USERNAME=.*/DB_USERNAME=daemon/" .env
+sed -i '' "s/DB_PASSWORD=.*/DB_PASSWORD=7DmHdDctBI6?/" .env
+
+composer install
+php artisan key:generate
+php artisan migrate --seed
+
+php artisan storage:link
+
+find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
+chgrp -R www-data storage bootstrap/cache
+chmod -R ug+rwx storage bootstrap/cache
+
+################
+
+heading "⛓️‍💥 Configuring links..."
+
+read -p "Enter link path: /var/www/" PATH 
+ln -s public /var/www/${PATH}
+
+################
+
+heading "🚀 Deployment complete!"
