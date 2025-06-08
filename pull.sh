eb587b499e81af878456ee087bcec5f2f2725bca
Wojciech Przybyła <contact@wpww.pl>
2025-06-08 15:52:18 +0200

    ⭐ fixed breaking migration pulling

--- pull.sh
+++ pull.sh
@@ -65,12 +65,12 @@ update() {
   if [ -d "$folder/.git" ]; then
     local git_output
     local pull_status
-    git_output = $(git pull)
-    pull_status = $?
+    git_output=$(git pull)
+    pull_status=$?
     if [ $? -ne 0 ]; then
       git reset --hard
-      git_output = $(git pull)
-      pull_status = $?
+      git_output=$(git pull)
+      pull_status=$?
     fi
 
     # if repo is up to date, don't do anything
