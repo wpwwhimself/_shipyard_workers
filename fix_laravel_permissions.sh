89bc518de633da33e360bdc88e05450a4737a5bb
Wojciech Przybyła <contact@wpww.pl>
2025-09-19 23:28:59 +0200

    🩹 fix_laravel_permissions | more folders

--- fix_laravel_permissions.sh
+++ fix_laravel_permissions.sh
@@ -1,4 +1,4 @@
 find . -type f -exec chmod 644 {} \;
 find . -type d -exec chmod 755 {} \;
-chgrp -R www-data storage bootstrap/cache
-chmod -R ug+rwx storage bootstrap/cache
+chgrp -R www-data storage bootstrap/cache public database
+chmod -R ug+rwx storage bootstrap/cache public database
